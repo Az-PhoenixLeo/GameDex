ee3b41499beecace568daa2859a2b6bf1f473000
Az-PhoenixLeo <rocio@iMac-de-Centro.local>
2018-01-15 14:36:16 -0600

    avance prueba de fb

--- pruebafb/index.js
+++ pruebafb/index.js
@@ -13,4 +13,29 @@ app.use(session({
 	saveUninitialized: true
 }));
 
+var FACEBOOK_APP_ID = '1027871424017894',
+	FACEBOOK_APP_SECRET = '07b28004a49bae13fa5be3c234727ced';
+
+var fbOpts = {
+	clientID: FACEBOOK_APP_ID,
+	clientSecret: FACEBOOK_APP_SECRET,
+	callbackURL: 'http://localhost:8080/auth/facebook/callback'
+};
+
+var fbCallback = function(accessToken, refreshToken, profile, cb){
+
+};
+
+passport.use(new FacebookStrategy(fbOpts, fbCallback));
+
+app.route('/')
+	.get(function(req, res){
+		res.send('hello foo bar')
+	});
+
+app.route('/auth/facebook/callback')
+	.get(function(req, res){
+		res.send('esta cosa checa el status de la petición. :p !');
+	})
+
 app.listen(8080);
